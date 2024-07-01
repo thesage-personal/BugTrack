75de04f36b5505b1e05c0b7793b353d4f88def05
thesage-personal <109345367+thesage-personal@users.noreply.github.com>
2024-07-01 10:21:08 +0530

    initial Commit

---
+++ Program.cs
@@ -0,0 +1,46 @@
+using Newtonsoft.Json.Serialization; //imported Newton.Json
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+//for npgsql datetime null
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehaviour", true);
+
+// Add services to the container.
+
+builder.Services.AddControllersWithViews();
+
+//Add CORS
+builder.Services.AddCors(c =>
+{
+    c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
+
+//JSON Serializer
+builder.Services.AddControllersWithViews().AddNewtonsoftJson(options => options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore)
+    .AddNewtonsoftJson(options => options.SerializerSettings.ContractResolver = new DefaultContractResolver());
+
+var app = builder.Build();
+
+// Enable CORS
+app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller}/{action=Index}/{id?}");
+
+app.MapFallbackToFile("index.html"); ;
+
+app.Run();
